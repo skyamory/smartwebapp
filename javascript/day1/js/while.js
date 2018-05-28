b9196a7acb11fc38d4ab01e73f2f7786744d7f85
Unknown <els-enora@hanmail.net>
2018-05-28 15:02:48 +0900

    javascript 1회차: while+for+do while문 예제

---
+++ javascript/day1/js/while.js
@@ -0,0 +1,20 @@
+var i=1;
+var sum=0;
+while(i<=10){
+    console.log("i :"+i);
+    sum+=i;
+    i++;
+}
+console.log("sum :"+sum);
+
+for(i=1;i<=10;i++){
+    console.log("i :"+i);
+}
+
+i=11;
+sum=0;
+do{
+    console.log("i :"+i);
+    i++;
+}while(i<=10);
+console.log("sum :"+sum);
